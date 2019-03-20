f62f6a4e0551c9786413bcb88f458596267645b6
Silcrana <silvana.vieira@hotmail.com>
2019-03-20 14:28:23 -0300

    add lazyload images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { LazyLoadImagesModule } from 'ngx-lazy-load-images';
-import { LazyLoadImageModule, intersectionObserverPreset } from 'ng-lazyload-image';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 import { AppRoutingModule } from './app-routing.module';
-import { StudentModule } from './student/student.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    StudentModule,
-    LazyLoadImageModule.forRoot({
-      preset: intersectionObserverPreset
-    })
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, LazyLoadImageModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,9 +5,9 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styles: [
     `
       img {
-        min-width: 1497px;
+        min-width: 1024px;
         width: 100%;
-        min-height: 1127px;
+        min-height: 500px;
       }
 
       img.ng-lazyloaded {
@@ -25,8 +25,15 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
     `
   ],
   template: `
-    <img *ngFor="let image of images" [src]="image" />
-    <br><br>
+    <figure class="figure">
+      <img
+        class="figure-img img-fluid rounded"
+        *ngFor="let image of images"
+        [defaultImage]="defaultImage"
+        [errorImage]="errorImage"
+        [lazyLoad]="image"
+      />
+    </figure>
   `
 })
 export class HomeComponent {
@@ -39,8 +46,11 @@ export class HomeComponent {
     'https://images.unsplash.com/photo-1471201187657-6406da15e43b?dpr=2&auto=compress,format&fit=crop&w=1199&h=1199&q=80',
     'https://images.unsplash.com/photo-1483488792196-78bedff29c21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-    'https://images.unsplash.com/photo-1493406300581-484b937cdc41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-
+    'https://images.unsplash.com/photo-1493406300581-484b937cdc41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    'https://assets.imgix.net/unsplash/bridge.jpg?auto=compress&w=900&h=600&fit=crop&crop=focalpoint&fp-debug=1&fp-x=.41&fp-y=.43',
+    'https://assets.imgix.net/unsplash/pineneedles.jpg?fp-y=.43&fp-x=.41&fp-debug=1&crop=focalpoint&fit=crop&h=600&w=900&auto=compress',
+    'https://assets.imgix.net/unsplash/turntable.jpg?fp-y=.43&fp-x=.41&fp-debug=1&crop=focalpoint&fit=crop&h=600&w=900&auto=compress',
+    'https://assets.imgix.net/unsplash/transport.jpg?fp-y=.43&fp-x=.41&fp-debug=1&crop=focalpoint&fit=crop&h=600&w=900&auto=compress'
   ];
 }
 
